0f9bcefc0f51249921be2e29dafd2949d111c1dd
Ryan Smith <Franzferdinan51@gmail.com>
2020-03-22 20:19:19 -0400

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,43 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import styled from "styled-components";
 
+const Nav = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  padding-bottom:20px;
+  justify-content:center;
+`
+
+const Button = styled.button`
+  border: 1px solid black;
+  border-radius: 6px;
+  background-color:white;
+  color: black;
+  font-size: 20px;
+  padding:10px;
+  margin:10px
+  padding-bottom:30px;
+  &&:hover{
+  background: black;
+  color:white
+  }
+  width:120px
+`
 export default function Header() {
   return (
-    <header className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+    // <header className="ui centered">
+    //   <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+
+    <header>
+      <h1>Rick & Morty Fan Page</h1>
+      <Nav>
+        <Link to='/'><Button>Home</Button></Link>
+        <Link to='/Search'><Button>Search</Button></Link>
+        <Link to='/Characters'><Button>Characters</Button></Link>
+      </Nav>
+
+
     </header>
   );
 }
